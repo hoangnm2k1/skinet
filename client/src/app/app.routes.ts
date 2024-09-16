94645ed617494e5a20c311749f66a2cffe9453c2
hoangnm2k1 <hoangnm2k1@gmail.com>
2024-09-16 17:32:39 +0700

    End of section 10

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -4,8 +4,8 @@ import { ShopComponent } from './features/shop/shop.component';
 import { ProductDetailsComponent } from './features/shop/product-details/product-details.component';
 
 export const routes: Routes = [
-  // { path: '', component: HomeComponent },
-  // { path: 'shop', component: ShopComponent },
-  // { path: 'shop/:id', component: ProductDetailsComponent },
-  // { path: '**', redirectTo: '', pathMatch: 'full' }
+  { path: '', component: HomeComponent },
+  { path: 'shop', component: ShopComponent },
+  { path: 'shop/:id', component: ProductDetailsComponent },
+  { path: '**', redirectTo: '', pathMatch: 'full' }
 ];
